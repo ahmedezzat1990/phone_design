50b88f83a8a9e0206b79c650d8458720d4768126
ahmedezzat1990 <121746908+ahmedezzat1990@users.noreply.github.com>
2023-09-25 20:16:45 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.img');
+function phones(phone){
+  img.src = phone
+}
+let container = document.querySelector('.container');
+function colors (color){
+  container.style.background = color;
+}
